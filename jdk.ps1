715b807dd2e9629b50a6b52a18aa2488c1ec6ea2
Igor Akkerman <commit@igorakkerman.de>
2021-03-19 23:08:31 +0100

    Powershell JDK switcher script added

---
+++ jdk.ps1
@@ -0,0 +1,23 @@
+function global:jdk11 {
+  $env:JAVA_HOME = "C:\Lokal\IT\Java\jdk-11.0.2"
+}
+function global:jdk12 {
+  $env:JAVA_HOME = "C:\Lokal\IT\Java\jdk-12"
+}
+function global:jdk13 {
+  $env:JAVA_HOME = "C:\Lokal\IT\Java\jdk-13"
+}
+function global:jdk14 {
+  $env:JAVA_HOME = "C:\Lokal\IT\Java\jdk-14.0.2"
+}
+function global:jdk15 {
+  $env:JAVA_HOME = "C:\Lokal\IT\Java\jdk-15.0.2"
+}
+
+function global:jdk16 {
+  $env:JAVA_HOME = "C:\Lokal\IT\Java\jdk-16"
+}
+
+function global:jdklatest {
+  $env:JAVA_HOME = ""
+}
