0ea2dccd8502f8af59f2c6d916a65f47034176dd
Igor Akkerman <commit@igorakkerman.de>
2021-09-21 12:28:17 +0200

    configured JDK automatically added to path

--- jdk.ps1
+++ jdk.ps1
@@ -1,29 +1,41 @@
+function global:Add-Jdk-To-Path {
+    $env:PATH = ($env:JAVA_HOME + '\bin;') + ($env:PATH -replace 'C:\\Lokal\\IT\\Java\\.+?;', '')
+}
+
 function global:jdk11 {
   $env:JAVA_HOME = "C:\Lokal\IT\Java\jdk-11.0.2"
+  Add-Jdk-To-Path
 }
 function global:jdk12 {
   $env:JAVA_HOME = "C:\Lokal\IT\Java\jdk-12"
+  Add-Jdk-To-Path
 }
 function global:jdk13 {
   $env:JAVA_HOME = "C:\Lokal\IT\Java\jdk-13"
+  Add-Jdk-To-Path
 }
 function global:jdk14 {
   $env:JAVA_HOME = "C:\Lokal\IT\Java\jdk-14.0.2"
+  Add-Jdk-To-Path
 }
 function global:jdk15 {
   $env:JAVA_HOME = "C:\Lokal\IT\Java\jdk-15.0.2"
+  Add-Jdk-To-Path
 }
 
 function global:jdk16 {
   $env:JAVA_HOME = "C:\Lokal\IT\Java\jdk-16.0.2"
+  Add-Jdk-To-Path
 }
 
 function global:jdk17 {
   $env:JAVA_HOME = "C:\Lokal\IT\Java\jdk-17"
+  Add-Jdk-To-Path
 }
 
 function global:jdkcorretto {
   $env:JAVA_HOME = "C:\Lokal\IT\Java\corretto-1.8.0_282"
+  Add-Jdk-To-Path
 }
 
 function global:jdklatest {
@@ -31,4 +43,3 @@ function global:jdklatest {
 }
 
 jdklatest
-$env:PATH = $env:PATH + ";" + $env:JAVA_HOME + "\bin"
